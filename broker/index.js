1692ab98a4860d2394790f676be35ea35e3e5bfd
gshong <hgs33hgs@gmail.com>
2020-07-26 14:15:06 +0900

    테스트 케이스 배열 추가

---
+++ broker/index.js
@@ -0,0 +1,113 @@
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const exec = require('child_process').execFile;
+
+let fileInfo = {
+    cpp: {
+        language: 'cpp',
+        filename: 'cpp_mergesort.exe',
+        result: 'result_cpp.txt'
+    }
+}
+
+const numTests = 2;
+
+const testCaseList = [
+    {
+        jobSize: 100000000,
+        maxValue: 100000000,
+        numThreads: 1
+    },
+    {
+        jobSize: 100000000,
+        maxValue: 100000000,
+        numThreads: 2
+    },
+    {
+        jobSize: 100000000,
+        maxValue: 100000000,
+        numThreads: 4
+    },
+    {
+        jobSize: 100000000,
+        maxValue: 100000000,
+        numThreads: 8
+    },
+    {
+        jobSize: 100000000,
+        maxValue: 100000000,
+        numThreads: 16
+    }
+]
+
+async function execSomeFile(filename, jobSize, maxValue, numThreads){
+    return new Promise(async (resolve, reject)=>{
+        exec(filename, [jobSize, maxValue, numThreads], (err, stdout) => {
+            if(err){
+                reject(err);
+            }
+            resolve(stdout);
+        })
+    });
+}
+
+async function startTest(filename, testCase){
+    for(let i =0;i<numTests;i++){
+        await execSomeFile(filename, testCase.jobSize, testCase.maxValue, testCase.numThreads);
+    }
+}
+
+function collectResult(filename){
+    if(fs.existsSync(filename) === false){
+        console.error(`${filename} does not exist`);
+        return;
+    }
+
+    let result = fs.readFileSync(filename, 'utf8').toString();
+    //console.log(result);
+    let eachDataList = result.split('\n');
+    let average = 0;
+    let numData = 0;
+    for(let eachData of eachDataList){
+        if(eachData.length === 0) continue;
+        average += Number(eachData);
+        numData++;
+    }
+    average = average / numData;
+    console.log(`average : ${average}`);
+
+    return average;
+}
+
+async function startTestLang(langInfo){
+    if(langInfo === undefined || langInfo === null){
+        console.error(`langInfo does not exist`);
+        return;
+    }
+
+    console.log(`${langInfo.language} test START`);
+
+    let results = [];
+    let cnt = 0;
+
+    for(let testCase of testCaseList){
+        console.log(`${langInfo.language} ${cnt++} test case START`);
+        //INFO: 기존 실험 파일 제거
+        if(fs.existsSync(langInfo.result)){
+            fs.unlinkSync(langInfo.result);
+        }
+
+        await startTest(langInfo.filename, testCase);
+        results.push(collectResult(langInfo.result));
+    }
+
+    return results;
+}
+
+async function main() {
+    let cppResults = await startTestLang(fileInfo.cpp);
+    console.log(cppResults);
+}
+
+main();
